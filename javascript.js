99229723519ce608a07e457498e6f7943e80d662
Carlos Echeverria <ca.echeverriam@duocuc.cl>
2024-05-12 16:07:56 -0400

    Orden 12-05-24

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const btnLeft = document.querySelector(".btn-left");
     const btnRight = document.querySelector(".btn-right");
     const slider = document.querySelector(".carruseles");
@@ -35,101 +35,100 @@ document.addEventListener("DOMContentLoaded", function() {
     btnLeft.addEventListener("click", moveToLeft);
     btnRight.addEventListener("click", moveToRight);
 
-    // Inicializar el carrusel
     updateSlider();
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
     $("#logins").validate({
-    rules: {
-        nombre: {
-            required: true,
-            minlength: 4,
-            maxlength: 12
+        rules: {
+            nombre: {
+                required: true,
+                minlength: 4,
+                maxlength: 12
+            },
+            password: {
+                required: true,
+                minlength: 4,
+                maxlength: 8
+            }
         },
-        password: {
-            required: true,
-            minlength: 4,
-            maxlength: 8
-        }
-    },
-    messages: {
-        nombre: {
-            required: "Por favor, introduzca su nombre",
-            minlength: "El nombre debe tener al menos 4 caracteres.",
-            maxlength: "El nombre no debe tener más de 12 caracteres."
+        messages: {
+            nombre: {
+                required: "Por favor, introduzca su nombre",
+                minlength: "El nombre debe tener al menos 4 caracteres.",
+                maxlength: "El nombre no debe tener más de 12 caracteres."
+            },
+            password: {
+                required: "Por favor proporcione una contraseña",
+                minlength: "La contraseña debe tener al menos 4 caracteres.",
+                maxlength: "La contraseña no debe tener más de 8 caracteres."
+            }
         },
-        password: {
-            required: "Por favor proporcione una contraseña",
-            minlength: "La contraseña debe tener al menos 4 caracteres.",
-            maxlength: "La contraseña no debe tener más de 8 caracteres."
+        errorPlacement: function (error, element) {
+            error.insertAfter(element.parent());
         }
-    },
-    errorPlacement: function(error, element) {
-        error.insertAfter(element.parent());
-    }
-});
+    });
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
     $("#registros").validate({
 
-    messages: {
-        nombre: {
-            required: "Por favor, introduzca su nombre",
-            minlength: "El nombre debe tener al menos 4 caracteres.",
-            maxlength: "El nombre no debe tener más de 12 caracteres."
+        messages: {
+            nombre: {
+                required: "Por favor, introduzca su nombre",
+                minlength: "El nombre debe tener al menos 4 caracteres.",
+                maxlength: "El nombre no debe tener más de 12 caracteres."
+            },
+            password: {
+                required: "Por favor proporcione una contraseña",
+                minlength: "La contraseña debe tener al menos 4 caracteres.",
+                maxlength: "La contraseña no debe tener más de 8 caracteres."
+            },
+            mail: {
+                minlength: "El E-mail debe tener al menos 11 caracteres.",
+                required: true
+            }
         },
-        password: {
-            required: "Por favor proporcione una contraseña",
-            minlength: "La contraseña debe tener al menos 4 caracteres.",
-            maxlength: "La contraseña no debe tener más de 8 caracteres."
-        },
-        mail: {
-            minlength: "El E-mail debe tener al menos 11 caracteres.",
-            required: true
+        errorPlacement: function (error, element) {
+            error.insertAfter(element.parent());
         }
-    },
-    errorPlacement: function(error, element) {
-        error.insertAfter(element.parent());
-    }
-});
+    });
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
     $("#contacto").validate({
-    rules: {
-        nombre: {
-            required: true,
-            minlength: 4,
-            maxlength: 12
+        rules: {
+            nombre: {
+                required: true,
+                minlength: 4,
+                maxlength: 12
+            },
+            mensaje: {
+                required: true,
+                maxlength: 200
+            },
+            mail: {
+                required: true
+            }
         },
-        mensaje: {
-            required: true,
-            maxlength: 200
+        messages: {
+            nombre: {
+                required: "Por favor, introduzca su nombre",
+                minlength: "El nombre debe tener al menos 4 caracteres.",
+                maxlength: "El nombre no debe tener más de 12 caracteres."
+            },
+            msg: {
+                required: "Por favor, introduzca un mensaje.",
+                maxlength: "El mensaje no debe tener más de 200 caracteres."
+            },
+            mail: {
+                required: "Por favor, introduzca un correo electrónico."
+            }
         },
-        mail: {
-            required: true
+        errorPlacement: function (error, element) {
+            error.insertAfter(element.parent());
         }
-    },
-    messages: {
-        nombre: {
-            required: "Por favor, introduzca su nombre",
-            minlength: "El nombre debe tener al menos 4 caracteres.",
-            maxlength: "El nombre no debe tener más de 12 caracteres."
-        },
-        msg: {
-            required: "Por favor, introduzca un mensaje.",
-            maxlength: "El mensaje no debe tener más de 200 caracteres."
-        },
-        mail: {
-            required: "Por favor, introduzca un correo electrónico."
-        }
-    },
-    errorPlacement: function(error, element) {
-        error.insertAfter(element.parent());
-    }
-});
+    });
 });
 
 function initializeSearch() {
@@ -139,12 +138,12 @@ function initializeSearch() {
     if (searchInput && boxSearch) {
         const items = boxSearch.querySelectorAll('li');
 
-        searchInput.addEventListener('input', function(event) {
+        searchInput.addEventListener('input', function (event) {
             const searchText = event.target.value.trim().toLowerCase();
 
             let hasResults = false;
 
-            items.forEach(function(item) {
+            items.forEach(function (item) {
                 const text = item.textContent.trim().toLowerCase();
 
                 if (text.includes(searchText)) {
@@ -162,7 +161,7 @@ function initializeSearch() {
             }
         });
 
-        document.addEventListener('click', function(event) {
+        document.addEventListener('click', function (event) {
             if (!boxSearch.contains(event.target) && event.target !== searchInput) {
                 boxSearch.style.display = 'none';
             }
